030d68d7c35590cf4fe307059ebdbd4016b86278
moritaka <moritaka325@163.com>
2020-11-23 23:07:07 +0800

    fix some bugs.

--- src/index.ts
+++ src/index.ts
@@ -22,10 +22,12 @@ async function main() {
 
   let is_added = false;
 
+  const add_to_cart = await add_to_cart_request(product_id);
+
   while (!is_added) {
     logger.debug(`正在将产品${product_id}加入购物车`);
     // 
-    const cart_res = await add_to_cart_request(product_id);
+    const cart_res = await select_in_cart_req(product_id);
     const body = JSON.parse(cart_res.parsed_body);
     is_added = is_target_add_to_order(body);
 
@@ -74,11 +76,52 @@ interface JDApiConfig {
   body: string;
 }
 
+async function refresh_cart() {
+  const url = "https://cart.jd.com/";
+
+  const res = await fetch(url, {
+    headers: {
+      cookie: COOKIE,
+      "user-agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36",
+      Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+      "Connection": "keep-alive",
+      Host: "cart.jd.com",
+      Referer: "https://item.jd.com/",
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "same-site",
+    },
+    method: "GET",
+  });
+
+  return new Promise((resolve, reject) => {
+    if (!res.ok) {
+      reject(res);
+    }
+
+    let response_body = "";
+
+    res.body.on("data", function (d) {
+      response_body += d.toString();
+    });
+
+    res.body.on("end", function () {
+      resolve({
+        parsed_body: response_body,
+        response: res,
+      });
+    });
+  });
+}
+
 async function send_js_api_request(api_config: JDApiConfig) {
   const url = "https://api.m.jd.com/api";
 
   const res = await fetch(url, {
-    body: api_config.body,
+    // body: api_config.body,
     // credentials: "include",
     headers: {
       "content-type": "application/x-www-form-urlencoded",
@@ -94,6 +137,50 @@ async function send_js_api_request(api_config: JDApiConfig) {
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36",
     },
     method: "POST",
+  });
+
+  return new Promise((resolve, reject) => {
+    if (!res.ok) {
+      reject(res);
+    }
+
+    let response_body = "";
+
+    res.body.on("data", function (d) {
+      response_body += d.toString();
+    });
+
+    res.body.on("end", function () {
+      resolve({
+        parsed_body: response_body,
+        response: res,
+      });
+    });
+  });
+}
+
+async function add_to_cart_request(product_id: string) {
+
+  // const url = `https://cart.jd.com/addToCart.html?rcd=1&pid=${product_id}&pc=1&eb=1&em=`
+
+  const url = `https://cart.jd.com/gate.action?pid=${product_id}&pcount=1&ptype=1`
+
+  const res = await fetch(url, {
+    headers: {
+      cookie: COOKIE,
+      "user-agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36",
+      Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+      "Connection": "keep-alive",
+      Host: "cart.jd.com",
+      Referer: "https://item.jd.com/",
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "same-site",
+    },
+    method: "GET",
   });
 
   return new Promise((resolve, reject) => {
@@ -134,19 +221,19 @@ async function uncheck_all() {
   const body = {
     functionId: "pcCart_jc_cartUnCheckAll",
     appid: "JDC_mall_cart",
+    loginType: 3,
     body: data,
   };
 
   const res = await fetch(url, {
     body: querystring(body),
-    // credentials: "include",
     headers: {
       "content-type": "application/x-www-form-urlencoded",
       accept: "application/json, text/plain, */*",
       cookie: COOKIE,
-      origin: "https://trade.jd.com",
+      origin: "https://cart.jd.com",
+      referer: "https://cart.jd.com/",
       pragma: "no-cache",
-      referer: "https://trade.jd.com/shopping/order/getOrderInfo.action",
       "sec-fetch-dest": "empty",
       "sec-fetch-mode": "cors",
       "sec-fetch-site": "same-site",
@@ -183,7 +270,7 @@ async function uncheck_all() {
  * @param {string} [skuUuid]
  * @return {*}  {Promise<{ parsed_body: string; response: NodeFetchResponse }>}
  */
-async function add_to_cart_request(
+async function select_in_cart_req(
   id: string,
   skuUuid?: string
 ): Promise<{ parsed_body: string; response: NodeFetchResponse }> {
